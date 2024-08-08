afc9b79ffc6f4ae3b548ca24748617d7da81d697
qwasium <113295196+qwasium@users.noreply.github.com>
2024-08-08 14:30:59 +0900

    bump version

--- src/ugokukun/__init__.py
+++ src/ugokukun/__init__.py
@@ -1,6 +1,6 @@
 """Info about the package."""
 
-__version__ = "0.1.0"
+__version__ = "0.1.1"
 __author__ = "qwasium"
 __license__ = "Apache-2.0"
 __url__ = "https://github.com/qwasium/ugoku-kun/tree/main"
